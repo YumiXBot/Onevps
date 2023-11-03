93a05337de16cbf970b5c828bb76266cfd14df25
˹ʏᴜᴍɪ ꭙ˼ <146187449+YumiXBot@users.noreply.github.com>
2023-11-03 15:10:21 +0530

    Update thumbnails.py

--- AloneXMusic/utils/thumbnails.py
+++ AloneXMusic/utils/thumbnails.py
@@ -1,6 +1,11 @@
 import os
+import re
+
 import aiohttp
 import aiofiles
+from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
+from unidecode import unidecode
+from youtubesearchpython.__future__ import VideosSearch
 
 from AloneXMusic import app
 from config import YOUTUBE_IMG_URL
